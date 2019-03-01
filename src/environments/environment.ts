5586ae5f75c9da2b141fee5b9322b73f941987b3
Тригубов Евгений Игоревич (ДИТ) <evgentrigub@yandex.ru>
2019-03-01 19:05:05 +0300

    web environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:4000'
-  apiUrl: 'https://finbotproject.azurewebsites.net'
+  apiUrl: 'http://localhost:4000'
+  // apiUrl: 'https://finbotproject.azurewebsites.net'
 };
