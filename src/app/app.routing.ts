82d1a145d56c6a98ad3213d4caec45226c2dcc87
Unknown <evgentrigub@yandex.ru>
2019-03-02 10:44:14 +0300

    отключил вход

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,10 +7,12 @@ import { SettingsComponent } from './settings/settings.component';
 import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
-    {path: 'dashboard', component: MainDashboardComponent, canActivate: [AuthGuard]},
-    {path: 'settings', component: SettingsComponent , canActivate: [AuthGuard]},
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+    {path: 'dashboard', component: MainDashboardComponent},
+    {path: 'settings', component: SettingsComponent},
+    // {path: 'dashboard', component: MainDashboardComponent, canActivate: [AuthGuard]},
+    // {path: 'settings', component: SettingsComponent , canActivate: [AuthGuard]},
+    // { path: 'login', component: LoginComponent },
+    // { path: 'register', component: RegisterComponent },
     { path: 'about', component: AboutComponent },
     { path: '**', redirectTo: 'dashboard' }
 ];
