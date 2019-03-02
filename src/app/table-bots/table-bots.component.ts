92123d323384ec19cddf445d088da7c9a7c3fe70
Unknown <evgentrigub@yandex.ru>
2019-03-02 14:22:05 +0300

    fix

--- src/app/table-bots/table-bots.component.ts
+++ src/app/table-bots/table-bots.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 import { TradingBot } from '../trading-bot-model';
 
+
+const bots: TradingBot[] = [
+  
+]
+
 @Component({
   selector: 'app-table-bots',
   templateUrl: './table-bots.component.html',
   styleUrls: ['./table-bots.component.css']
 })
-
-const bots: TradingBot[] = [
-  // {}
-]
 
 export class TableBotsComponent implements OnInit {
 
