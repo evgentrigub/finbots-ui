7a79e7805bec3cb554ad6f11c944aced24c12538
Unknown <evgentrigub@yandex.ru>
2019-04-28 21:42:21 +0300

    formGroup для тестов

--- src/app/test-for-level-risk/test-for-level-risk.component.ts
+++ src/app/test-for-level-risk/test-for-level-risk.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import { Question } from '../Models/Questions';
 import { TaskService } from '../services/task.service';
 import { MatRadioChange } from '@angular/material';
@@ -12,16 +12,20 @@ import { Answers } from '../Models/Answers';
 })
 export class TestForLevelRiskComponent implements OnInit {
 
-  dataFrom: FormGroup;
-  questions: Question[];
+  questionsAnswers: Question[];
   rating = 0;
   selection: Answers;
 
+  readonly formGroup: FormGroup;
+  readonly questionsControl: FormArray;
+
   constructor(
     private taskService: TaskService,
+    private formBuilder: FormBuilder
   ) {
-    this.dataFrom = new FormGroup({
-      'answers': new FormControl('', Validators.required)
+    this.questionsControl = this.formBuilder.array([]);
+    this.formGroup = this.formBuilder.group({
+      questions: this.questionsControl
     });
    }
 
@@ -30,13 +34,33 @@ export class TestForLevelRiskComponent implements OnInit {
   }
 
   getQuestions() {
-    this.taskService.getQuestionsList().subscribe(r => {
-      this.questions = r;
+    this.taskService.getQuestionsList().subscribe((results: Question[]) => {
+
+      while (this.questionsControl.length !== 0) {
+        this.questionsControl.removeAt(0);
+      }
+
+      this.formGroup.reset();
+      if (!results) {
+        this.questionsAnswers = [];
+        return;
+      }
+      this.questionsAnswers = results;
+
+      results
+      .map(res => this.createFormGroupQuestion(res))
+      .forEach(el => {
+        this.questionsControl.push(el);
+      });
     });
   }
 
-  radioChange(event: MatRadioChange) {
-    this.rating = this.rating + event.value.answerRate;
+  createFormGroupQuestion(question: Question): FormGroup {
+    return this.formBuilder.group({
+      question: this.formBuilder.control(question.objContent),
+      answers: this.formBuilder.control(question.answers),
+      answer: this.formBuilder.control('', [Validators.required])
+    });
   }
 
   /**
@@ -44,10 +68,7 @@ export class TestForLevelRiskComponent implements OnInit {
    * и отправляет его в статистику пользователя на бек
    */
   submit() {
-    if (this.dataFrom.valid) {console.log('fdf')}
+    if (this.formGroup.valid) {console.log('form is valid'); }
 
   }
-
-
-
 }
